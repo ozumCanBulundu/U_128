dafb3c91c8933a59335979e04acbde72ba1a8f21
Ozum Can BULUNDU <ozumcanbulundu@gmail.com>
2023-07-02 11:58:41 +0300

    mainMenu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    } 
+
+    public void StartGame() 
+    {
+        SceneManager.LoadScene(1);
+        
+    }
+
+}
